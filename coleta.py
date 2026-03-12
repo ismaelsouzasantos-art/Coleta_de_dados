741cf29d32f925a55734b9957fe3b864222ab868
ismaelsouzasantos-art <ismael.souza.santos@escola.pr.gov.br>
2026-03-12 20:21:39 -0300

    Add files via upload

---
+++ coleta.py
@@ -0,0 +1,10 @@
+#Solicita o nome do usuário
+nome = input("Digite seu nome: ")
+
+#Solicita a idade do usuário
+idade = input("Digite sua idade: ")
+
+#Exibe as informações coletadas
+print("Olá " , nome , " você tem " , idade , " anos.")
+
+#print("Olá " + nome + " você tem " + idade + " anos.")
